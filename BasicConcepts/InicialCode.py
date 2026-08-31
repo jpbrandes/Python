8a0324062b8a5be0fbaaecd948a8cc509eba2586
jpbrandes <jpdevprojects879@gmail.com>
2026-08-31 10:55:48 -0300

    Add a basic python program

---
+++ BasicConcepts/InicialCode.py
@@ -0,0 +1,35 @@
+print("1. Write some code that outputs your name to the screen") 
+
+my_name = input("What is your name: ")
+print(f'{my_name}')
+
+print("2. Write some code that outputs your name to the screen 10 times using math")
+
+name = input("What is yout name: ")
+print(f'{name}' * 10)
+
+print("3. Write some code that asks how many Apples you'd like to purchase and then outputs the response")
+
+apples_number = input("How many apples you like to buy: ")
+print(f'{apples_number}' )
+
+print("4. Write some code that asks for a person's first name on one line, then after they type it in, asks for their last name. Then tells them 'Hello first last name'")
+
+first_name = input("What is yout first name: ")
+last_name = input("What is yout last name: ")
+
+print(f'Hello {first_name} {last_name}')
+
+print("5. Write some code that asks (one line at a time) what your name is, where you live, what your phone number is, and what your favorite color is; then outputs all that info to the screen one item per line.")
+
+name_ = input("What is your name: ")
+print(f'{name_}')
+
+location = input("Where do you live: ")
+print(f'{location}')
+
+phone_number = input("What is your phone number: ")
+print(f'{phone_number}')
+
+favourite_color = input("What is your favorite color: ")
+print(f'{favourite_color}')
