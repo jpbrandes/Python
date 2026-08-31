58e6f123e145b189d8dd03bcc04968d7aba8a6e4
jpbrandes <jpdevprojects879@gmail.com>
2026-08-31 16:37:37 -0300

    Add conditional file and folder

---
+++ Conditional/NameAsk.py
@@ -0,0 +1,7 @@
+first_name = input("Type your first name: ")
+
+last_name = input("Type your last name: ")
+
+print(f'Your name is {first_name.capitalize()} {last_name.capitalize()}') 
+
+print(f'Your name {first_name} {last_name} has {len(first_name + last_name)} characters.')
--- README.md
+++ README.md
@@ -6,5 +6,7 @@ That repository is for Python code.
 ## Scripts
 A folder that contains scripts for various use cases.
 
+---
+
 ## Variable
 A basic Python folder that teaches basic variable manipulation.
