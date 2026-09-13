049c7d01d05911593438895aca9aff376d9c0467
jpbrandes <jpdevprojects879@gmail.com>
2026-09-13 12:20:35 -0300

    add program to sum with his antecessor

---
+++ Loops/CummulativeLoop.py
@@ -0,0 +1,5 @@
+def Sum_currentNum_minus_previousnumber():
+    for current_num in range(1,10):
+        print(f'The current sum the current number {current_num} and previos number {current_num - 1} = {current_num + (current_num - 1)})')
+
+Sum_currentNum_minus_previousnumber()
