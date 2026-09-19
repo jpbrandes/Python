d944525304a41d5b98fd76af7f0878ef489de28d
jpbrandes <jpdevprojecets879@gmail.com>
2026-09-19 17:18:03 -0300

    Add program that Slice strings

---
+++ Lists/StringSlicing.py
@@ -0,0 +1,8 @@
+def StringSlicing():
+    string = input("Type your desired string: ")
+    reversed_string = string[::-1]
+
+    print(f"Original string: {string}")
+    print(f"Reversed string: {reversed_string}")
+
+StringSlicing()    
